264346aad9582767ad295a6c089a40fda4ad443c
salexs95 <salexs95@yandex.ru>
2020-06-30 01:56:25 +0300

    Tokenomica fork

---
+++ ccxt/tokenomika_assets.py
@@ -0,0 +1,85 @@
+TESTNET_ASSETS = {
+    "WAVES": {
+        "id": "WAVES",
+        "precision": 8
+    },
+    "BTC": {
+        "id": "Emn8cyGDFgnLCKLTXqVWhKJARhtR2muBUuZaSmqNzDfn",
+        "precision": 8
+    },
+    "ETH": {
+        "id": "Co11Tbj83TeZCnuXrZQwv6Bs4XG2sm1vGQiQ4FLDjJWs",
+        "precision": 8
+    },
+    "USD": {
+        "id": "C7NpjX1zJ1JrSGw8AvpBwfwUHU44a2u6mhCjdkJgXgrT",
+        "precision": 8
+    },
+    "WEST": {
+        "id": "AMFteLfPzPhTsFc3NfvHG7fSRUnsp3tJXPH88G1PCisT",
+        "precision": 8
+    },
+    "TEURS": {
+        "id": "CtuwNwubHNVwhw1FznxwQ77DPaaTrL6MsqQtJHip79XA",
+        "precision": 8
+    },
+    "XRP": {
+        "id": "HUdXNRE4VcCx64PCPYwh6KL2cxvRaKcR8bXe3Ar9fG4p",
+        "precision": 6
+    },
+    "TEUR": {
+        "id": "DeSwLim3LSwUGFj2YmERbK3Qf6LWV1k9YczS7LnniWFr",
+        "precision": 8
+    }
+}
+
+TESTNET_PAIRS = [
+    ("WAVES", "BTC"),
+    ("BTC", "TEUR"),
+    ("BTC", "USD"),
+    ("ETH", "BTC"),
+    ("ETH", "TEUR"),
+    ("WEST", "BTC"),
+    ("WEST", "WAVES"),
+    ("WEST", "USD"),
+    ("XRP", "WAVES")
+]
+
+MAINNET_ASSETS = {
+    "WAVES": {
+        "id": "WAVES",
+        "precision": 8
+    },
+    "BTC": {
+        "id": "9SxLVHaEGTeEjRiAMnEw74YWWndQDRw8SZhknK9EYoUd",
+        "precision": 8
+    },
+    "ETH": {
+        "id": "HnEtQ7kZCbpa8gDMtkKE1qkvqZVhQBR6iEo1xMoX1abv",
+        "precision": 8
+    },
+    "USD": {
+        "id": "ARUnrACYathMJwq6gPvnb9kW2FHXTYLaVbsKwB2JC1DH",
+        "precision": 8
+    },
+    "WEST": {
+        "id": "4LHHvYGNKJUg5hj65aGD5vgScvCBmLpdRFtjokvCjSL8",
+        "precision": 8
+    },
+    "TEUR": {
+        "id": "3EXKWnU1QJfLa4bW5Dyw5hF5aPo289UrgfodjaExHAfN",
+        "precision": 8
+    }
+}
+
+MAINNET_PAIRS = [
+    ("WAVES", "BTC"),
+    ("BTC", "TEUR"),
+    ("BTC", "USD"),
+    ("ETH", "BTC"),
+    ("ETH", "TEUR"),
+    ("WEST", "BTC"),
+    ("WEST", "WAVES"),
+    ("WEST", "BTC"),
+
+]
